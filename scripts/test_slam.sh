9d22f3952ec2f129ddfc1d874ba58b3fd1a6370d
Nataliya <nataliya.mishukova@gmail.com>
2021-05-23 17:23:07 +0000

    feat: Add test_slam.sh script

---
+++ scripts/test_slam.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+xterm -e " roslaunch my_robot world.launch" &
+sleep 20
+xterm -e " roslaunch my_robot gmapping.launch" &
+sleep 15
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 15
+xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch"
+ 
